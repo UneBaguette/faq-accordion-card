4309baf7b5c3524292884d8b843ebe321e431280
UneBaguette <thomasiger42@gmail.com>
2022-01-25 11:54:06 +0100

    making this thing actually usable.

---
+++ script.js
@@ -0,0 +1,34 @@
+const title = document.querySelectorAll('.title');
+const titre = document.querySelectorAll('.title > h3');
+const span = document.querySelectorAll('.text > span');
+const imgs = document.querySelectorAll('.title > img');
+let h3;
+let spans;
+let arrow;
+
+title.forEach(titles => {
+    titles.addEventListener('click', function(){
+    })
+});
+
+
+for (let i = 0; i < title.length; i++){
+    let titles = title[i];
+    titles.addEventListener('click' , function() {
+        h3 = titre[i];
+        spans = span[i];
+        arrow = imgs[i];
+        if (!h3.classList.contains("active") && !spans.classList.contains("activespan") && !arrow.classList.contains("activearrow")) {
+            h3.classList.add("active");
+            spans.classList.add("activespan");
+            arrow.classList.add("activearrow");
+        }
+        else {
+            h3.classList.remove("active");
+            spans.classList.remove("activespan");
+            arrow.classList.remove("activearrow");
+        }
+    })
+}
+
+
